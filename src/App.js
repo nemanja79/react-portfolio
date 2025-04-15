fa125be7fbdd900b347f71180b05fe6c91abb923
nemanja79 <nemanja.todorovic@pm.me>
2025-04-15 22:22:02 +0200

    added CV(Portfolio)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import { Projection } from 'leaflet';
+import Portfolio from './components/Portfolio';
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
